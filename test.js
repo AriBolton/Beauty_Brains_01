487f0482b98519953e8a949bba18a2d95249ff3b
aribolton <ari.bolton2011@gmail.com>
2024-12-10 16:02:33 -0800

    phase 1

---
+++ test.js
@@ -0,0 +1,10 @@
+/**
+ * Pseudocode
+ */
+
+var bananaHeight= function() {
+    return (document.getElementByID ("myNumber").value/7);
+}
+function myCalculation() {
+    document.getElementByID ("display").innerhtml = bananaHeight();
+}
